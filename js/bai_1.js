7b141ffe0881dcb6856bdf96c8122409b797a799
Dungchv13 <Dungutc13@gmail.com>
2020-09-10 08:24:28 +0700

    add toan_tu_ba_ngoi

---
+++ js/bai_1.js
@@ -0,0 +1,5 @@
+let a=parseFloat(prompt("hay nhap a"));
+let b=parseFloat(prompt("hay nhap b"));
+
+let result = ((a+b)<4) ? "below" : "over";
+alert("ket qua la "+result);
---
+++ js/bai_2.js
@@ -0,0 +1,5 @@
+let a=prompt("hello");
+
+let message = (a=="employee") ? "hello": (a=="director") ? "greetings" : (a=="") ? 'no login': "";
+
+alert(message);
